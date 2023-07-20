feb485fbae0b9c830b86928f0dba485c386f8e56
Debankushg <debankushghosh@gamil.com>
2023-07-20 09:32:59 +0530

    components_props_emitEvents has been done

---
+++ newProject/src/components/Popup.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>This is Pop Up Component</h2>
+        <input type="text" v-model="name" />
+        <button @click="$emit('close', name)">Close Popup</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Popup',
+    emits: {
+        close: (name) => {
+            if (!name) {
+                return false
+            } else {
+                return true
+            }
+        }
+    },
+    data() {
+        return {
+            name: '',
+        }
+    },
+}
+</script>
+
+<style scoped></style>
---
+++ newProject/src/components/Props.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>
+        Hello {{name}} you are {{ heroName }}
+    </h2>
+</template>
+
+<script>
+    export default {
+       name:'Props',
+       props:['name', 'heroName'], 
+    }
+</script>
+
+<style scoped>
+
+</style>
